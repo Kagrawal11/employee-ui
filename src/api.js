dea46cffbf41e5f5c2a4e4f82b9751f3af60c754
Kartik <k03agrawal@gmail.com>
2025-12-07 21:40:37 +0530

    base URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
- baseURL: "https://employee-api-imqn.onrender.com/employee",
+   baseURL: "https://employee-api-sew5.onrender.com/employee",
+// baseURL: "https://employee-api-imqn.onrender.com/employee",
  //baseURL: "http://localhost:5000/employee"
 });
